9051abac75b8e8e5e9e8a2a15407d832e3115d62
Vraj Patel <vrajp1052005@gmail.com>
2025-02-17 22:37:18 -0500

    Update stock_utils.py

--- utils/stock_utils.py
+++ utils/stock_utils.py
@@ -7,45 +7,59 @@ import streamlit.components.v1 as components
 
 # chart for stock
 def create_stock_chart(symbol):
+    st.markdown("""
+    <style>
+    body {
+        background-color: #000000;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
         # here we are 
-        tradingview_html = f"""
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-            </head>
-            <body>
-                <div class="tradingview-widget-container">
-                    <div id="tradingview_chart"></div>
-                    <script type="text/javascript">
-                    new TradingView.widget(
-                    {{
-                        "width": "100%",
-                        "height": 600,
-                        "symbol": "NASDAQ:{symbol}",
-                        "interval": "D",
-                        "timezone": "Etc/UTC",
-                        "theme": "dark",
-                        "style": "1",
-                        "locale": "en",
-                        "toolbar_bg": "#131722",
-                        "enable_publishing": false,
-                        "allow_symbol_change": true,
-                        "container_id": "tradingview_chart"
-                    }});
-                    </script>
-                </div>
-            </body>
-            </html>
-        """
+    tradingview_html = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+        </head>
+        <body>
+            <div class="tradingview-widget-container">
+                <div id="tradingview_chart"></div>
+                <script type="text/javascript">
+                new TradingView.widget(
+                {{
+                    "width": "100%",
+                    "height": 600,
+                    "symbol": "NASDAQ:{symbol}",
+                    "interval": "D",
+                    "timezone": "Etc/UTC",
+                    "theme": "dark",
+                    "style": "1",
+                    "locale": "en",
+                    "toolbar_bg": "#131722",
+                    "enable_publishing": false,
+                    "allow_symbol_change": true,
+                    "container_id": "tradingview_chart"
+                }});
+                </script>
+            </div>
+        </body>
+        </html>
+    """
 
-        # Display TradingView chart in Streamlit
-        components.html(tradingview_html, height=650)
+    # Display TradingView chart in Streamlit
+    components.html(tradingview_html, height=650)
 
 
 # chart for crypto..
 def create_crypto_chart(symbol):
+    st.markdown("""
+    <style>
+    body {
+        background-color: #000000;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
     # TradingView Chart Embedded using st.components.html()
     tradingview_html = f"""
